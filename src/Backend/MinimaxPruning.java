0e9dc2bcc29ae28213383d11ef1299a2dbce44e8
Mariemyoussri <61235438+Mariemyoussri@users.noreply.github.com>
2021-12-06 19:03:12 +0200

    .

--- src/Backend/MinimaxPruning.java
+++ src/Backend/MinimaxPruning.java
@@ -5,4 +5,12 @@ public class MinimaxPruning implements IMinimax{
     public State Decision(State initial, int level) {
         return null;
     }
+
+    private EvaluationState maximize(State state, int level){
+        return null;
+    }
+
+    private EvaluationState minimize(State state, int level){
+        return null;
+    }
 }
