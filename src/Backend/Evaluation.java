1b18a8077b200d43a613bd4d5e4b7b38053a71cb
noha-ahmed <noha.morsy2000@gmail.com>
2021-12-07 20:06:05 +0200

    Evaluation class added

---
+++ src/Backend/Evaluation.java
@@ -0,0 +1,25 @@
+package Backend;
+
+public class Evaluation {
+    public static int evaluateScore(int[][] board){
+        return 0;
+    }
+
+    private static int evalHorizantalWindow(int[][] board, int start){
+        return 0;
+    }
+
+    private static int evalVerticalWindow(int[][] board, int start){
+        return 0;
+    }
+
+    private static int evalPDiagonalWindow(int[][] board, int startRow, int startColumn ){
+        return 0;
+    }
+
+    private static int evalNDiagonalWindow(int[][] board, int startRow, int startColumn){
+        return 0;
+    }
+
+
+}
